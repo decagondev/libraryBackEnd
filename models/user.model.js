1025648270661907428084c07abf33964c3003d2
Tom Tarpey <tomtarpeydev@gmail.com>
2024-07-20 00:34:06 +0100

    define field name: String, (not nullable)

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,10 @@ const User = sequelize.define('User', {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
 });
 
