8e512e278858f931f7e55421493532ae60c2561e
Tom Tarpey <tomtarpeydev@gmail.com>
2024-07-20 00:30:32 +0100

    define field id: Integer (primary key)

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config.js');
 
 const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  }
 });
 
 
